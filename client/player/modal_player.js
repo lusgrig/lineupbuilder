92f58b3ba6b575e35eea897e277ee9311f8f642d
Lusine <lusine@Lusines-MacBook-Pro.local>
2014-09-24 20:50:47 -0700

    AddPlayerWorks

--- client/player/modal_player.js
+++ client/player/modal_player.js
@@ -30,21 +30,23 @@ Template.tModalPlayer.events({
     var firstName = $('.first-name').val();
     var gameStatus = $('.game-status').val();
     var fieldPosition = $('.field-position').val();
+    var emailPosition = $('.email-position').val();
     var gerseyNumber = $('.gersey-number').val();
     var seasonFeeOwed = $('.season-fee-owed').val();
     var seasonFeePaid = $('.season-fee-paid').val();
-    updatePlayer(firstName, gameStatus, fieldPosition, gerseyNumber, seasonFeeOwed, seasonFeePaid);
+    updatePlayer(firstName, gameStatus, fieldPosition, emailPosition, gerseyNumber, seasonFeeOwed, seasonFeePaid);
     $("#modal-id").modal("hide");
     Session.set('sPlayerId', null);
   }
 });
 
-var updatePlayer = function(firstName, gameStatus, fieldPosition, gerseyNumber, seasonFeeOwed, seasonFeePaid) {
+var updatePlayer = function(firstName, gameStatus, fieldPosition, emailPosition, gerseyNumber, seasonFeeOwed, seasonFeePaid) {
   Players.update(Session.get("sPlayerId"), {
     $set: {
       firstName: firstName,
       gameStatus: gameStatus,
       fieldPosition: fieldPosition,
+      emailPosition: emailPosition,
       gerseyNumber: gerseyNumber,
       seasonFeeOwed: seasonFeeOwed,
       seasonFeePaid: seasonFeePaid
